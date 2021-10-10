a4ddd71734dedaa0da56881891348f3e6225b4fe
Shashwat Srivastava <72192705+srivastavashashwat05@users.noreply.github.com>
2021-10-10 20:09:07 +0530

    Add files via upload

---
+++ Shashwat/practice2.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+
+public class practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        float a = 7.0f/4 * 9 / 2;
+//        System.out.println(a);
+//        System.out.println("**********************************************");
+//
+//        System.out.println("Enter your grade: ");
+//        char grade = 'B';
+//        grade +=8;
+//        System.out.println("Encrypted grade is :  " + grade);
+//        grade -=8;
+//        System.out.println("Decrypted grade is :  " + grade);
+//
+//        System.out.println("**********************************************");
+//
+//        System.out.println("Enter number: ");
+//        int b= 488;
+//        int a1 = sc.nextInt();
+//
+//        if(b>a1){
+//            System.out.println("Yes, given is greater");
+//        }else{
+//            System.out.println("No");
+//        }
+//        System.out.println("**********************************************");
+//
+//        int v=6;
+//        int u=2;
+//        int n=2;
+//        int s=2;
+//
+//        int result = ((v*v)-(u*u))/(2*n*s);
+//        System.out.println(result);
+
+           int x=7;
+           int m = 7*49/7+35/7;
+        System.out.println(m);
+    }
+}
